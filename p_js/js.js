c90cf78e1582918ea778704a976d5d47e7a913a9
Lc <apple@Lc-2.local>
2016-12-04 23:06:53 -0500

    first commit

---
+++ p_js/js.js
@@ -0,0 +1,23 @@
+function cover_i(){
+  document.getElementById('index').style.backgroundImage = "url('" + './p_image/cover.jpg' + "')";
+}
+function cover_d(){
+  document.getElementById('index').style.backgroundImage = "url('" + './p_image/cover_d.jpg' + "')";
+}
+function cover_m(){
+  document.getElementById('index').style.backgroundImage = "url('" + './p_image/cover_m.jpg' + "')";
+}
+function cover_g(){
+  document.getElementById('index').style.backgroundImage = "url('" + './p_image/cover_g.jpg' + "')";
+}
+function cover_a(){
+  document.getElementById('index').style.backgroundImage = "url('" + './p_image/cover_a.jpg' + "')";
+}
+function show_nav(){
+  document.getElementById('full').style.opacity='1';
+  document.getElementById('full').style.display='block';
+}
+function show_page(){
+  document.getElementById('full').style.display='none';
+  document.getElementById('full').style.opacity='0';
+}
